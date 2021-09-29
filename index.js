a53eab8528cc248273837062d413a29b8605fccc
retsepile <Retshepilekoloko27@gmail.com>
2021-09-29 14:29:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const fortunes = require('./data/fortunes.json')
+
+const port = 3000;
+
+const app = express();
+
+app.get('/fortunes', (req, res) => {   
+    res.json(fortunes);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
